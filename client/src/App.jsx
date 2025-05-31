bde9241e8aed8d48bf9cf13006f44126213f8ddf
DKH707 <derekkh707@gmail.com>
2025-05-30 19:10:14 -0500

    Remove Expereince and Coffee Page temporarily

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,7 @@ import NotFoundPage from './Components/NotFoundPage'
 import AboutPage from './Components/AboutPage'
 import ProjectsPage from './Components/ProjectsPage'
 import ContactPage from './Components/ContactPage'
-import ExperiencePage from './Components/ExperiencePage.jsx'
 import Navigation from './Components/Navigation.jsx'
-import CoffeePage from './Components/CoffeePage.jsx'
 
 function App() {
   return (
@@ -23,8 +21,6 @@ function App() {
                 <Route path='/' element={<HomePage />} />
                 <Route path='/about' element={<AboutPage />} />
                 <Route path='/projects' element={<ProjectsPage />} />
-                <Route path='/experience' element={<ExperiencePage />} />
-                <Route path='/coffee' element={<CoffeePage />} />
                 <Route path='/contact' element={<ContactPage />} />
                 <Route path="*" element={<NotFoundPage />} />
               </Route>
